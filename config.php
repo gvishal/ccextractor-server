bc74fa1a489caf1d3a59cd710f8cc73f0712410d
Ruslan Kuchumov <kuchumovri@gmail.com>
2014-08-04 22:25:58 +0000

    view_cc.php

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+$cfg = parse_ini_file("./server.ini");
+
+if (!array_key_exists("mysql_host", $cfg))
+	$cfg["mysql_host"] = "localhost";
+
+if (!array_key_exists("mysql_user", $cfg))
+	$cfg["mysql_user"] = "root";
+
+if (!array_key_exists("mysql_password", $cfg))
+	$cfg["mysql_password"] = "root";
+
+if (!array_key_exists("mysql_db_name", $cfg))
+	$cfg["mysql_db_name"] = "cce";
+
+if (!array_key_exists("archive_files_dir", $cfg))
+	$cfg["archive_files_dir"] = "./cc";
+
+if (!array_key_exists("buffer_files_dir", $cfg))
+	$cfg["buffer_files_dir"] = "./tmp";
+
+define('INT_LEN',         10);
+
+define('CONN_CLOSED',     101);
+define('PROGRAM_ID',      102);
+define('PROGRAM_NEW',     103);
+define('PROGRAM_CHANGED', 106);
+define('PROGRAM_DIR',     108);
+define('CAPTIONS',        104);
+define('XDS',             105);
+define('DOWNLOAD_LINKS',  201);
+
+define('OFFSET',          800);
+?>
--- view_cc.php
+++ view_cc.php
@@ -7,43 +7,6 @@ if (!array_key_exists("id", $_GET))
 $id = intval($_GET["id"]);
 if (0 == $id)
 	return;
-
-function links($id)
-{
-	date_default_timezone_set("UTC");
-
-	$cfg = parse_ini_file("./server.ini");
-	if (!array_key_exists("archive_files_dir", $cfg))
-		$cfg["archive_files_dir"] = "./cc";
-
-	$info_file = $cfg["archive_files_dir"] . "/" . date("Y/m-M/d/") . ARCH_INFO_FILENAME;
-
-	if (!file_exists($info_file))
-		return;
-
-	if (($fp = fopen($info_file, "r")) == 0)
-		return;
-
-	while (1) {
-		fgetc($fp); 
-		if (feof($fp)) 
-			break;
-		fseek($fp, -1, SEEK_CUR);
-
-		$line = fgets($fp);
-
-		sscanf($line, "%d %d %s %s %s %s", $t, $cur_id, $addres, $bin, $txt, $srt);
-
-		if ($cur_id != $id)
-			continue;
-
-		echo "<a href=\"" . $bin . "\">bin</a> ";
-		echo "<a href=\"" . $txt . "\">txt</a> ";
-		echo "<a href=\"" . $srt . "\">srt</a> ";
-	}
-
-	fclose($fp);
-}
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -51,7 +14,7 @@ function links($id)
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8; no-store">
 	<script type="text/javascript" src="jquery-2.1.1.min.js"></script>
-	<script type="text/javascript">
+<script type="text/javascript">
 var CONN_CLOSED =     101;
 var CAPTIONS =        104;
 var XDS =             105;
@@ -59,77 +22,80 @@ var DOWNLOAD_LINKS =  201;
 var PROGRAM_NEW =     103;
 var PROGRAM_CHANGED = 106;
 
-// define('CONN_CLOSED',     101);
-// define('PROGRAM_ID',      102);
-// define('PROGRAM_NEW',     103);
-// define('PROGRAM_CHANGED', 106);
-// define('PROGRAM_START',   108);
-// define('CAPTIONS',        104);
-// define('XDS',             105);
-// define('DOWNLOAD_LINKS',  201);
-
 var rc;
 var cur_links = "";
 $(function() {
 	var line = 0;
+	update();
 	rc = setInterval(update, 1000);
 
 	function update() {
 		var done = false;
 		$.ajax({
-			type: "GET",
+		type: "GET",
 			url: "cc.php",
 			dataType: "json",
 			data: {
-				id: "<?= $id ?>",
+			id: "<?= $id ?>",
 				st: line
-			},
-			success: function(data) {
-				$.each(data, function(i, d) {
-					if (d.command == CAPTIONS) {
-						$("<div id=\"cc_line\">" + d.data + "</div>").prependTo("#cc");
-					} else if (d.command == XDS && $('#xds').prop('checked')) {
-						$("<div id=\"cc_line\">" + d.data + "</div>").prependTo("#cc");
-					} else if (d.command == CONN_CLOSED) {
-						$( "<div id=\"new_program\">Connection closed</div>").prependTo("#cc");
+		},
+		success: function(data) {
+			$.each(data, function(i, d) {
+				if (d.command == CAPTIONS) {
+					$("<div id=\"cc_line\">" + d.data + "</div>").prependTo("#cc");
+				} else if (d.command == XDS && $('#xds_chkbox').prop('checked')) {
+					$("<div id=\"cc_line\">" + d.data + "</div>").prependTo("#cc");
+				} else if (d.command == CONN_CLOSED) {
+					$( "<div id=\"conn_closed_cc\">Connection closed</div>").prependTo("#cc");
 
-						clearInterval(rc);
-					} else if (d.command == PROGRAM_CHANGED) {
-						$(
-							"<div id=\"new_program\">Program changed: " + 
-							"<span id=\"prg_name\">" + d.data + "</span>" +
-							"</div>" +
-							"<div id=\"cc_link\">Download links: " + cur_links + "</div>"
-						).prependTo("#cc");
-						cur_links = "";
-					} else if (d.command == PROGRAM_NEW) {
-						$(
-							"<div id=\"new_program\">Program name: " + 
-							"<span id=\"prg_name\">" + d.data + "</span>" +
-							"</div>"
-						).prependTo("#cc");
-						cur_links = "";
-					} else if (d.command == DOWNLOAD_LINKS) {
-						cur_links += "<a href=\"" + d.filepath + "\">" + d.name + "</a> ";
-					}
+					clearInterval(rc);
+					$("#ctrl").remove();
 
-					if (d.command == CAPTIONS || d.command == XDS)
-						line = parseInt(d.line) + 1;
-				});
-			}
+				} else if (d.command == PROGRAM_NEW || d.command == PROGRAM_CHANGED) {
+					$(
+						"<div id=\"new_program\">Program name: " + 
+						"<span id=\"pr_name_cc\">" + d.data + "</span>" +
+						"</div>"
+					).prependTo("#cc");
+
+					if (d.command == PROGRAM_CHANGED)
+						$( "<div id=\"cc_link\">Download links: " + cur_links + "</div>").prependTo("#cc");
+
+					cur_links = "";
+					$("#pr_name").html(d.data);
+				} else if (d.command == DOWNLOAD_LINKS) {
+					cur_links += "<a href=\"" + d.filepath + "\">" + d.name + "</a> ";
+					$("#pr_links").html(cur_links);
+				}
+
+				if (d.command == CAPTIONS || d.command == XDS)
+					line = parseInt(d.line) + 1;
+			});
+		}
 		});
 	}
 });
-	</script>
+</script>
+<link rel="stylesheet" type="text/css" href="view_cc.css">
 </head>
 
 <body>
-	<div id="ctrl">
-		<input type="checkbox" checked id="xds"> Print XDS
-		<? links($id) ?>
+<div id="ctrl">
+	<div id="name_box">
+		<div id="name_lbl">Current program: </div>
+		<div id="pr_name"></div>
 	</div>
-	<div id="cc">
+	<div id="links_box"> 
+		<div id="links_lbl">Download links: </div>
+		<div id="pr_links"></div>
+	</div>
+	<div id="xds_box">
+		<input type="checkbox" checked id="xds_chkbox">
+		<div id="xds_lbl"> Print XDS </div>
+	</div>
+</div>
 
-	</div>
+<div id="cc">
+</div>
 </body>
 </html>
